147b89663acce2ca4355bace52f69401af7dc7ff
adriaanph <aph@ska.ac.za>
2024-04-24 20:27:59 +0200

    renamed modules, no change

--- dvs/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-    Top-level package of analysis routines for the Dish Verification System.
-"""
-import analyze_fastgain as fastgain
-import analyze_SEFD as driftscan
-import analyze_tipping as tipcurve
-
-cbid2url = lambda cbid: "http://archive-gw-1.kat.ac.za/%s/%s_sdp_l0.full.rdb"%(cbid,cbid) # Only works from inside the SARAO firewall
