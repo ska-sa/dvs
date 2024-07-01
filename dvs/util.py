aaec941ffe9d5c53a203c629939d94131f01d4c6
adriaanph <aph@ska.ac.za>
2024-07-01 16:39:07 +0200

    hack_dataset() 1) allow dataset to be specified as a URL.

--- dvs/util.py
+++ dvs/util.py
@@ -3,19 +3,22 @@
     
     @author: aph@sarao.ac.za
 """
+import katdal
 
 
 cbid2url = lambda cbid: "http://archive-gw-1.kat.ac.za/%s/%s_sdp_l0.full.rdb"%(cbid,cbid) # Only works from inside the SARAO firewall
 
 
-def hack_dataset(dataset, hackedL=False, ant_rx_override=None, verbose=False):
+def hack_dataset(dataset, hackedL=False, ant_rx_override=None, verbose=False, **kwargs):
     """ Typical modifications to datasets that were generated with the DVS.
         Use like 'ds = hack_dataset(katdal.open(URL), ...)'
         
-        @param dataset: the katdal dataset to modify in-situ
+        @param dataset: the katdal dataset to modify in-situ, or a URL to open as a katdal dataset.
         @param hackedL: True if the dataset was generated with the hacked L-band digitiser i.e. sampled in 1st Nyquist zone.
         @param ant_rx_override: {ant:rx_serial} to override (default None)
         @return: the opened dataset. """
+    dataset = katdal.open(dataset, **kwargs) if isinstance(dataset,str) else dataset
+    
     if hackedL: # Change centre freq and flip channel/frequency mapping
         for spw in dataset.spectral_windows:
             spw.__init__(856e6/2., spw.channel_width, spw.num_chans, spw.product, sideband=-1)
