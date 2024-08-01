24c3aad67633a7567dbbf18944bf6d4adb0b30de
adriaanph <aph@ska.ac.za>
2024-08-01 17:26:22 +0200

    1) changed katsdpscripts from pip install to simple cloning 2) added soft link for katsdpscripts to dvs/libraries 3) automated the modification of dvs/kernel.json

--- install_venvs.sh
+++ install_venvs.sh
@@ -19,25 +19,28 @@ ipython3 kernel install --name "python3" --user
 pip install numpy scipy matplotlib
 pip install pysolr paramiko zernike
 
-pip install git+https://github.com/ska-sa/{scape,katdal,katversion,katpoint,katsdpscripts,katsdpcal}
+pip install git+https://github.com/ska-sa/{scape,katdal,katversion,katpoint,katsdpcal}
 pip install git+ssh://git@github.com/ska-sa/dvsholog
 # TODO: At present, the above package is private - a temporary situation. The workaround is to obtain a "zip" package and do:
 # pip install dvsholog-main.zip
 
 pip install git+https://github.com/telegraphic/PyGSM
-py=`ls ~/venv-py3/lib/`
-mv ~/venv-py3/lib/$py/site-packages/pygsm/gsm2016_components.h5 ~/venv-py3/lib/$py/site-packages/pygsm/gsm2016_components.h5~
-wget -O i~/venv-py3/lib/$py/site-packages/pygsm/gsm2016_components.h5 https://zenodo.org/record/3479985/files/gsm2016_components.h5?download=1
+PY=`ls ~/venv-py3/lib/`
+mv ~/venv-py3/lib/$PY/site-packages/pygsm/gsm2016_components.h5 ~/venv-py3/lib/$PY/site-packages/pygsm/gsm2016_components.h5~
+wget -O ~/venv-py3/lib/$PY/site-packages/pygsm/gsm2016_components.h5 https://zenodo.org/record/3479985/files/gsm2016_components.h5?download=1
 
 
 ## Set up the DVS workspace
-git clone git@github.com:ska-sa/dvs.git
+git clone https://github.com/ska-sa/dvs.git
+git clone https://github.com/ska-sa/katsdpscripts.git
 git clone git@github.com:ska-sa/systems-analysis.git
 # TODO: At present, the above package is private - a temporary situation. The workaround is to obtain a "zip" package and do:
 # unzip systems-analysis-master.zip -d systems-analysis
-ln -s `pwd`/systems-analysis/analysis dvs/libraries/analysis
+ln -f -s `pwd`/systems-analysis/analysis dvs/libraries/analysis
+ln -f -s `pwd`/katsdpscripts dvs/libraries/katsdpscripts
 
 ipython3 kernel install --name "dvs" --user
-jupyter kernelspec list | grep dvs
-echo ACTION: Insert the following line into the above dvs/kernel.json 
-echo  \"env\": {\"PYTHONPATH\":\"`pwd`/dvs\"},
+# Add a custom pythonpath
+KS=`jupyter kernelspec list | sed -n -E 's/\s*(dvs)\s+(\S+)/\2/p'`/kernel.json
+PK=\"env\":{\"PYTHONPATH\":\"`pwd`/dvs\"}
+sed -i -E ':a; N; $! ba; s@(.*)}@\1,'$PK'\n}@' $KS
