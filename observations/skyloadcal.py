43dd7bae9314e2eb91aabbafd112c6de0dd9f0ca
adriaanph <aph@ska.ac.za>
2024-08-02 20:44:27 +0200

    1) minor changes to clean up the code 2) added "--offset" argument (default 10deg) 3) removed hard-coded antenna

--- observations/skyloadcal.py
+++ observations/skyloadcal.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Track SCP for a specified time for hotbox tests.
+# Track the Moon and offset points from that, for a specified time.
 
 import time
 
@@ -11,33 +11,29 @@ import numpy as np
 
 
 # Set up standard script options
-description = 'Perform a mesurement of system tempreture using hot and cold ' \
-              'on sky loads. Over 6 frequency ranges. Note also some ' \
-              '**required** options below.'
-parser = standard_script_options(usage="%prog [options] hotload or coldload",
-                                 description=description)
+description = 'Perform a measurement of system temperature using hot and cold ' \
+              'on sky loads.'
+parser = standard_script_options(usage="%prog [options]", description=description)
 # Add experiment-specific options
 parser.add_option('-t', '--track-duration', type='float', default=30.0,
-                  help='Length of time for each loading, in seconds (default=%default)')
+                  help='Length of time for each pointing, in seconds (default=%default)')
 parser.add_option('-m', '--max-duration', type='float', default=-1,
-                  help='Maximum duration of script, in seconds (the default is to observing all sources once)')
+                  help='Maximum duration of script to repeat the measurement, in seconds ' \
+                  '(the default is to repeat the measurement just once)')
+parser.add_option('--offset', type='float', default=10.0,
+                  help='Offset from the Moon for "cold load" measurement, in degrees (default=%default)')
 # Set default value for any option (both standard and experiment-specific options)
 parser.set_defaults(description='Hotload and Coldload observation', dump_rate=1.0 / 0.512)
 # Parse the command line
 opts, args = parser.parse_args()
 
-if opts.description != 'Hotload and Coldload observation':
-    opts.description = 'Hotload and Coldload observation:' + opts.description
 
 nd_off = {'diode': 'coupler', 'on': 0., 'off': 0., 'period': -1.}
 nd_coupler = {'diode': 'coupler', 'on': opts.track_duration, 'off': 0., 'period': 0.}
 
-# if len(args) == 0:
-#     raise ValueError("Please specify the sources to observe as arguments, either as "
-#                      "description strings or catalogue filenames")
 
 with verify_and_connect(opts) as kat:
-    moon = kat.sources.lookup['moon']
+    moon = kat.sources.lookup['moon'][0]
     with start_session(kat, **vars(opts)) as session:
         session.standard_setup(**vars(opts))
         session.nd_params = nd_off
@@ -46,28 +42,21 @@ with verify_and_connect(opts) as kat:
         start_time = time.time()
         while once or time.time() < start_time + opts.max_duration:
             once = False
-            moon = katpoint.Target('Moon, special')
-            # find some way of getting this from session
-            antenna = katpoint.Antenna('ant1, -30:43:17.3, 21:24:38.5, 1038.0, 12.0, 18.4 -8.7 0.0, '
-                                       '-0:05:30.6 0 -0:00:03.3 0:02:14.2 0:00:01.6 -0:01:30.6 0:08:42.1, 1.22')
-            moon.antenna = antenna
-            off1_azel = katpoint.construct_azel_target(wrap_angle(moon.azel()[0] + np.radians(10)),
+            # TODO: avoid making an azel target: use radec target, with session.track_offset() instead 
+            off1_azel = katpoint.construct_azel_target(wrap_angle(moon.azel()[0] + np.radians(opts.offset)),
                                                        moon.azel()[1])
-            off1_azel.antenna = antenna
+            off1_azel.antenna = moon.antenna
             off1 = katpoint.construct_radec_target(off1_azel.radec()[0], off1_azel.radec()[1])
-            off1.antenna = antenna
+            off1.antenna = moon.antenna
             off1.name = 'off1'
 
-            off2_azel = katpoint.construct_azel_target(wrap_angle(moon.azel()[0] - np.radians(10)),
+            off2_azel = katpoint.construct_azel_target(wrap_angle(moon.azel()[0] - np.radians(opts.offset)),
                                                        moon.azel()[1])
-            off2_azel.antenna = antenna
+            off2_azel.antenna = moon.antenna
             off2 = katpoint.construct_radec_target(off2_azel.radec()[0], off2_azel.radec()[1])
-            off2.antenna = antenna
+            off2.antenna = moon.antenna
             off2.name = 'off2'
-            sources = katpoint.Catalogue(add_specials=False)
-            sources.add(moon)
-            sources.add(off2)
-            sources.add(off1)
+            sources = [moon, off2, off1]
             txtlist = ', '.join(["'%s'" % (target.name,) for target in sources])
             user_logger.info("Calibration targets are [%s]", txtlist)
             for target in sources:
