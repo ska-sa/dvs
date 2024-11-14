197434d1fea523e466381ff71a547e8efe09c95b
adriaanph <aph@ska.ac.za>
2024-11-15 00:48:54 +0200

    new way to consolidate hacks for observation scripts

---
+++ observations/_hacks_.py
@@ -0,0 +1,103 @@
+from katcorelib import user_logger
+import katpoint, time, numpy
+
+
+def apply_temp_hacks():
+    global session
+    try:
+        if not session.dry_run:
+            import tango
+            dsm = tango.DeviceProxy('10.96.66.100:10000/mid_dsh_0119/lmc/ds_manager')
+            dsm.tiltPointCorrEnabled = False
+    except Exception as e:
+        user_logger.warn("Failed to disable Tilt Corrections on Dish#119", e)
+    
+
+class start_session(object):
+    """ Hacked "no capture session" to ignore cbf & sdp """
+    def __enter__(self):
+        return self
+    def __exit(self, *a):
+        pass
+    def __init__(self, kat, *a, **k):
+        self.obs_params = {}
+        self.nd_params = {}
+        self.ants = kat.ants
+        kat.ants.set_sampling_strategy("lock", "event")
+        self.dry_run = kat.dry_run
+        self.telstate = self
+    def add(self, *a, **k): # For telstate
+        pass
+    def capture_start(self): # Ignored
+        pass
+    def standard_setup(self, *a, **k): # Ignored
+        pass
+    def set_target(self, target): # copied from https://github.com/ska-sa/katcorelib/blob/master/katcorelib/rts_session.py
+        if self.ants is None:
+            raise ValueError('No antennas specified for session - '
+                             'please run session.standard_setup first')
+        # Create references to allow easy copy-and-pasting from this function
+        ants = self.ants
+        # Convert description string to target object, or keep object as is
+        target = target if isinstance(target, katpoint.Target) else katpoint.Target(target)
+        
+        if self.dry_run: return True
+        for ant in ants:
+            # Don't set the target unnecessarily as this upsets the antenna
+            # proxy, causing momentary loss of lock
+            current_target = ant.sensor.target.get_value()
+            if target != current_target:
+                # Set the antenna target (antennas will already move there if in mode 'POINT')
+                ant.req.target(target.description)
+    def on_target(self, target): # copied from https://github.com/ska-sa/katcorelib/blob/master/katcorelib/rts_session.py
+        if self.ants is None:
+            return False
+        # Turn target object into description string (or use string as is)
+        target = getattr(target, 'description', target)
+        for ant in self.ants:
+            # Ignore disconnected antennas or ones with missing sensors
+            if (not ant.is_connected() or
+                    any([s not in ant.sensor for s in ('target', 'mode', 'lock')])):
+                continue
+            if (ant.sensor.target.get_value() != target or
+                    ant.sensor.mode.get_value() != 'POINT' or
+                    not ant.sensor.lock.get_value()):
+                return False
+        return True
+    def track(self, target, duration=20.0, announce=True): # copied from https://github.com/ska-sa/katcorelib/blob/master/katcorelib/rts_session.py
+        if self.ants is None:
+            raise ValueError('No antennas specified for session - '
+                             'please run session.standard_setup first')
+        # Create references to allow easy copy-and-pasting from this function
+        session, ants = self, self.ants
+        # Convert description string to target object, or keep object as is
+        target = target if isinstance(target, katpoint.Target) else katpoint.Target(target)
+
+        if announce:
+            user_logger.info("Initiating %g-second track on target '%s'",
+                             duration, target.name)
+        if self.dry_run: return True
+        
+        # This already sets antennas in motion if in mode POINT
+        session.set_target(target)
+
+        # Avoid slewing if we are already on target
+        if not session.on_target(target):
+            user_logger.info('slewing to target')
+            ants.req.mode('POINT')
+            # Wait until they are all in position (with 5 minute timeout)
+            ants.wait('lock', True, timeout=300)
+            user_logger.info('target reached')
+
+        user_logger.info('tracking target')
+        # Do nothing else for the duration of the track
+        time.sleep(duration)
+        user_logger.info('target tracked for %g seconds', duration)
+        return True
+    def load_scan(self, timestamp, az, el): # copied from https://github.com/ska-sa/katcorelib/blob/master/katcorelib/rts_session.py
+        samples = numpy.atleast_2d(numpy.c_[timestamp, az, el])
+        csv = '\n'.join([('%.3f,%.4f,%.4f' % (t, a, e)) for t, a, e in samples])
+        if not self.dry_run:
+            return self.ants.req.load_scan(csv)
+        else:
+            return True                    
