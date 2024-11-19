7dd3e33ecebbcfbc9a3570bccf996448a2541437
adriaanph <aph@ska.ac.za>
2024-11-19 12:52:11 +0200

    apply() 1) removed dsm.DisablePointingCorrections() because that gets automatically reset again at the DS level. that behaviour will have to be corrected at that level.

--- observations/_hacks_.py
+++ observations/_hacks_.py
@@ -107,7 +107,6 @@ def apply(kat):
             import tango
             dsm = tango.DeviceProxy('10.96.66.100:10000/mid_dsh_0119/lmc/ds_manager')
             dsm.tiltPointCorrEnabled = False
-            dsm.DisablePointingCorrections()
-            user_logger.warning("APPLIED HACK: Dish#119 Tilt & Pointing Corrections Disabled")
+            user_logger.warning("APPLIED HACK: Dish#119 Tilt Corrections Disabled")
         except Exception as e:
-            user_logger.warning("Failed to disable Corrections on Dish#119: %s" % e)
+            user_logger.warning("Failed to disable Tilt Corrections on Dish#119: %s" % e)
