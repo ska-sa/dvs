da9ca9cf6d1be8d50c0b9500a0651fed4cf4b80b
adriaanph <aph@ska.ac.za>
2024-12-04 20:14:12 +0200

    start_holog_session() 1) only update the eq_gain if it is > 2000 to start off with (default is 3234, so this would indicate there is manual intervention)

--- observations/dvs_obslib.py
+++ observations/dvs_obslib.py
@@ -216,9 +216,8 @@ def start_hacked_session(cam, **kwargs):
 
 def start_holog_session(cam, **kwargs):
     """ Start a capture session with standard hacks as required for proper operation of the DVS system,
-        including setting the requantisation gain to 
-        1. `standard_setup()` checks & updates Ku-band siggen frequency
-        2. `standard_setup()` disables pointing corrections for MKE Dish ACU's
+        including setting the requantisation gain to a value that is suitable for satellite observations
+        (to avoid X-engine accumulator numerical errors).
         
         @return: the session object.
     """
@@ -227,12 +226,15 @@ def start_holog_session(cam, **kwargs):
     if True: # TODO: only do this if the target list includes satellites
         old_eq = getattr(cam.cbf.sensors,"wide_antenna_channelised_voltage_%sv_eq"%cam.ants[0].name).get_value()
         old_eq = numpy.mean(eval(old_eq))
-        cam.cbf.req.wide_gain_all(500) # TODO: this assumes 4k, and ADC input of > -25dBFS? 
+        if (old_eq > 2000): # TODO: this assumes 4k, and ADC input of > -25dBFS? 
+            cam.cbf.req.wide_gain_all(500)
+            user_logger.info("Set EQ gain to 500!")
         
-        def exit_session(exc_type, exc_val, exc_tb):
-            cam.cbf.req.wide_gain_all(old_eq) # TODO: this assumes all inputs and channels had the same
-            session.__exit__(exc_type, exc_val, exc_tb)
-        session.__exit__ = exit_session
+            def exit_session(exc_type, exc_val, exc_tb):
+                cam.cbf.req.wide_gain_all(old_eq) # TODO: this assumes all inputs and channels had the same
+                user_logger.info("Restored EQ gain to original '%s'." % old_eq)
+                session.__exit__(exc_type, exc_val, exc_tb)
+            session.__exit__ = exit_session
     
     return session
 
