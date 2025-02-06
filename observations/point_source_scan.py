a2c8ac5a62c3059c9c0866b0e82ed8d309b37670
adriaanph <aph@ska.ac.za>
2025-02-06 18:11:42 +0200

    1) added 'skaku' style

--- observations/point_source_scan.py
+++ observations/point_source_scan.py
@@ -43,9 +43,10 @@ styles = {
     'skab1': dict(num_scans=9, scan_duration=60, scan_extent=6.6, scan_spacing=6.6/8, scan_in_az=[True]),
     'skab2': dict(num_scans=9, scan_duration=30, scan_extent=3.0, scan_spacing=3.0/8, scan_in_az=[True]),
     'skab3': dict(num_scans=9, scan_duration=24, scan_extent=1.8, scan_spacing=1.8/8, scan_in_az=[True]),
-    'skab3_AzEl': dict(num_scans=9, scan_duration=12, scan_extent=1.8, scan_spacing=1.8/8, scan_in_az=[True,False]),
+    'skab3_AzEl': dict(num_scans=9, scan_duration=12, scan_extent=1.8, scan_spacing=1.8/8, scan_in_az=[True,False]), # Experimental "basket weave"
     'skab4': dict(num_scans=9, scan_duration=24, scan_extent=1.0, scan_spacing=1.0/8, scan_in_az=[True]),
     'skab5a': dict(num_scans=9, scan_duration=24, scan_extent=0.6, scan_spacing=0.6/8, scan_in_az=[True]),
+    'skaku': dict(num_scans=13, scan_duration=13, scan_extent=0.5, scan_spacing=0.5/12, scan_in_az=[True]),
     # Proposed for SKA Dish: extent = 2.3*HPBWmax; speed = HPBW/3/1sec - to be done with 1sec dumps!
     '_skab1': dict(num_scans=21, scan_duration=21, scan_extent=9.0, scan_spacing=9.0/20, scan_in_az=[True]),
     '_skab2': dict(num_scans=13, scan_duration=13, scan_extent=3.3, scan_spacing=3.3/12, scan_in_az=[True]),
