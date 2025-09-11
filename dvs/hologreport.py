3c9e2b8bfba552d7c1cbadbcdcdf311975cf7c30
adriaanph <aph@ska.ac.za>
2025-09-11 20:27:51 +0200

    bugfix for previous commit.

--- dvs/hologreport.py
+++ dvs/hologreport.py
@@ -1302,7 +1302,7 @@ def plot_apmapdiffs(apmap0, apmap1, title, what="nopointingphasemap", vlim=None,
     unit = "mm" if ("dev" in what) else ("rad" if "phase" in what else "ampl")
     
     for ax_,apmap0,apmap1 in zip(axs,apmaps0,apmaps1):
-        t0, t1 = np.mean(apmap0.dataset.rawtime[apmap0.time_range]), np.mean(apmap1.dataset.rawtime[apmap1.time_range])
+        t0, t1 = np.mean(apmap0.dataset.rawtime[apmap0.dataset.time_range]), np.mean(apmap1.dataset.rawtime[apmap1.dataset.time_range])
         ax_[1].set_title("%.f - %.f"%(t0, t1), x=0)
         diff = apmap0.__getattribute__(what) - apmap1.__getattribute__(what)
         if masked:
