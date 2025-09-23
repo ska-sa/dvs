9a583998752e0426129076b37dcf9a74e22f3216
adriaanph <aph@ska.ac.za>
2025-09-23 14:53:21 +0200

    gen_scan() 1) now truncates time series to eliminate points in the past (with warning message)
    main:
    2) bugfix: first point as well as multiple cycles now start at exactly
    the correct time.
    3) break track table into segments of max length 1000, to avoid
    potential issue observed on 23/09/2025 when loading a ~4000 point table
    in one step.

--- observations/demo_load_scan.py
+++ observations/demo_load_scan.py
@@ -17,11 +17,11 @@ def load_load_scan_data(filename):
 
 
 def gen_scan(starttime, scan_pattern, clip_safety_margin=1.0, min_elevation=15., max_elevation=90.):
-    """ @param scan_data: [ [time0, az0, el0], ... [timeN, azN, elN] ] with time in unix timestamps, az & el in degrees
-        @return: (scan_data, duration, clipped) with scan_data as expected by session.load_scan()  """
+    """ @param starttime: Unix timestamp to assign to the first point.
+        @param scan_pattern: [ [time0, az0, el0], ... [timeN, azN, elN] ] with time in floating point seconds, az & el in degrees
+        @return: (scan_data, duration) with scan_data as expected by session.load_scan(), duration in seconds  """
     scan_data = np.array(scan_pattern, copy=True)
     scan_time, scan_az, scan_el = np.transpose(scan_data)
-    duration = scan_time[-1] - scan_time[0]
     
     timedata = starttime + (scan_time-scan_time[0])
     #clipping prevents antenna from hitting hard limit and getting stuck until technician reset it, 
@@ -31,8 +31,16 @@ def gen_scan(starttime, scan_pattern, clip_safety_margin=1.0, min_elevation=15.,
     scan_data[:,0] = timedata
     scan_data[:,1] = np.clip(np.nan_to_num(azdata),-180.0+clip_safety_margin,270.0-clip_safety_margin)
     scan_data[:,2] = np.clip(np.nan_to_num(eldata),min_elevation+clip_safety_margin,max_elevation-clip_safety_margin)
-    clipping_occurred = (np.sum(azdata==scan_data[:,1])+np.sum(eldata==scan_data[:,2])!=len(eldata)*2)
-    return scan_data, duration, clipping_occurred
+    clipping_occurred = (azdata!=scan_data[:,1]) | (eldata!=scan_data[:,2])
+    if np.any(clipping_occurred):
+        user_logger.warn("Clipping occurs in scan pattern - continuing with it clipped.")
+    valid_time = timedata > time.time()
+    if not np.all(valid_time):
+        user_logger.warn("Part of the scan pattern is in the past - continuing with it truncated.")
+        scan_data = scan_data[valid_time,:]
+    
+    duration = scan_data[-1,0] - scan_data[0,0]
+    return scan_data, duration
 
 
 # Set up standard script options
@@ -54,18 +62,16 @@ parser.set_defaults(description='Demonstration for load_scan', quorum=1.0, nd_pa
 
 # Parse the command line
 opts, args = parser.parse_args()
-T0 = 0
-if opts.T0:
-    try:
-        T0 = katpoint.Timestamp(opts.T0).secs
-    except Exception as e:
+try:
+    T0 = katpoint.Timestamp(opts.T0).secs
+except Exception as e:
+    if opts.T0:
         user_logger.warn("Specified start time could not be parsed - starting ASAP. Error is: %s" % e)
-    
+    T0 = -1
+
 # Load the pattern and confirm that it is suitable
 scan_pattern = load_load_scan_data(args[0])
-scan_data, cycle_expected_duration, clipping_occurred = gen_scan(0, scan_pattern, min_elevation=opts.horizon)
-if clipping_occurred:
-    user_logger.warn("Unexpected clipping occurred in scan pattern - continuing with it truncated.")
+scan_data, cycle_expected_duration = gen_scan(time.time()+1, scan_pattern, min_elevation=opts.horizon)
 
 
 # Check basic command-line options and obtain a kat object connected to the appropriate system
@@ -75,41 +81,47 @@ with verify_and_connect(opts) as kat:
     with start_session(kat, **vars(opts)) as session:
         session.standard_setup(**vars(opts))
         
-        user_logger.info("Initiating load_scan cycles (%s %g-second cycles)" % 
-                         (('unlimited' if opts.num_cycles<0 else '%d'%opts.num_cycles), cycle_expected_duration))
-
+        # Prepare for the first cycle
+        next_start = (time.time() + opts.prepopulatetime) if T0<0 else T0
+        assert (next_start + max(1,opts.num_cycles)*cycle_expected_duration > time.time()), "All track timestamps are in the past - aborting!"
+        
+        user_logger.info("Initiating load_scan cycles (%s %g-second cycles)", 
+                         ('unlimited' if opts.num_cycles<0 else '%d'%opts.num_cycles), cycle_expected_duration)
+        
         start_time = time.time()
         cycle = 0
-        while cycle<opts.num_cycles or opts.num_cycles<0: # Override exit conditions are coded in the next ten lines
+        while (cycle < opts.num_cycles) or (opts.num_cycles < 0): # Override exit conditions are coded in the next ten lines
             if (opts.max_duration is not None) and (time.time() - start_time >= opts.max_duration):
                 user_logger.warning("Maximum duration of %g seconds has elapsed - stopping script", opts.max_duration)
                 break
-            if opts.num_cycles<0:
-                user_logger.info("Performing scan cycle %d of unlimited", cycle + 1)
-            else:
-                user_logger.info("Performing scan cycle %d of %d", cycle + 1, opts.num_cycles)
-            user_logger.info("Using all antennas: %s",' '.join([ant.name for ant in session.ants]))
-            user_logger.info("Current scan estimated to complete at UT %s (in %.1f minutes)",time.ctime(time.time()+cycle_expected_duration+time.timezone),cycle_expected_duration/60.)
+            user_logger.info("Performing scan cycle %d of %s", cycle+1, ("unlimited" if opts.num_cycles<0 else '%d'%opts.num_cycles))
+            user_logger.info("Using all antennas: %s", ' '.join([ant.name for ant in session.ants]))
+            
+            # Scan pattern for this cycle. It's a demo, so simply use the exact same az, el for all antennas
+            scan_data, cycle_expected_duration = gen_scan(next_start, scan_pattern, min_elevation=opts.horizon)
+            user_logger.info("Current scan estimated to complete at UT %s (duration %.1f minutes)", time.ctime(next_start+cycle_expected_duration+time.timezone), cycle_expected_duration/60.)
             
             # Slew / unwrap to get to start position
             _, t_az, t_el = scan_data[0]
             user_logger.info("Performing azimuth unwrap to start position: %s,%s"%(t_az, t_el))
             azeltarget = katpoint.Target('azimuthunwrap,azel,%s,%s'%(t_az, t_el))
             session.track(azeltarget, duration=0, announce=False)
-
+            
             user_logger.info("Starting scan at %s, in %g seconds time" % (opts.T0, T0-time.time()))
             time.sleep(max(0, T0-time.time() - opts.prepopulatetime))
-            user_logger.info("Commencing scan now.")
-            # Update the pattern for this cycle, and go!
-            next_start = time.time() + opts.prepopulatetime
-            # It's a demo, simply use the exact same az, el for all antennas
-            scan_data = gen_scan(next_start, scan_pattern, min_elevation=opts.horizon)[0]
-            if not kat.dry_run:
-                session.load_scan(scan_data[:,0], scan_data[:,1], scan_data[:,2])
-            time.sleep(scan_data[-1,0]-time.time() - opts.prepopulatetime)
+            
+            user_logger.info("Commencing scan.")
+            for i in range(0, len(scan_data)//1000+1, 1): # Load in segments to avoid potential limitations and allow feedback
+                segment = slice(i*1000, min((i+1)*1000,len(scan_data)+1))
+                t_t, t_az, t_el = scan_data[segment,0], scan_data[segment,1], scan_data[segment,2]
+                user_logger.info("Loading segment %d: timestamps %.2f - %.2f", i, t_t[0], t_t[-1])
+                if not kat.dry_run:
+                    session.load_scan(t_t, t_az, t_el)
+                time.sleep(max(0, t_t[-1]-time.time() - opts.prepopulatetime))
+            # Prepare to seamlessly continue with the next cycle
+            next_start = t_t[-1] + (t_t[1]-t_t[0])
             
             cycle+=1
-            
             if (cycle > 1) and kat.dry_run:
                 user_logger.info("Testing only two cycles for dry-run")
                 break
